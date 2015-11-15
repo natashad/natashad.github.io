3a61d25037fbcd1a3ae12a98a61d4019560e9e6b
Natasha Dalal <natashadalal@gmail.com>
2015-11-15 20:22:50 +0530

    actually adding gulp

---
+++ dist/all.js
@@ -0,0 +1,22 @@
+$(function() {
+    setup_sidebar_controls();
+    responsive_sidebar();
+    $(window).on('resize', function() {
+        responsive_sidebar();
+    });
+});
+
+function responsive_sidebar() {
+    if ($(window).width() <= 768) {
+        $('.left-sidebar').removeClass('open');
+        $('.page').removeClass('sidebar-open');
+    }
+}
+
+function setup_sidebar_controls() {
+    $('.sidebar-close-button, .sidebar-open-button').click(function() {
+        $('.left-sidebar').removeClass('initial');
+        $('.page').toggleClass('sidebar-open');
+        $('.left-sidebar').toggleClass('open');
+    });
+}
